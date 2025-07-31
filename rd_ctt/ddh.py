0758078891faa49dd7d0bc1b09b7da6a39eb2670
kaz <kaz>
2025-07-31 11:38:16 -0400

    more

---
+++ rd_ctt/ddh.py
@@ -0,0 +1,11 @@
+RD_DDH_GUI_ALIVE = "ddh:th_alive"
+
+RD_DDH_BLE_SCAN_DUPLICATE = "ddh:ble:results"
+RD_DDH_BLE_ANTENNA = "ddh:ble:antenna"
+RD_DDH_BLE_TH_QUIT = "ddh:th_quit:ble"
+
+RD_DDH_GPS_TH_QUIT = "ddh:th_quit:gps"
+RD_DDH_GPS_FIX = "ddh:gps:results"
+RD_DDH_GPS_ANTENNA = "ddh:gps:antenna"
+RD_DDH_GPS_HAT_GFV = "ddh:gps:hat_gfv"
+RD_DDH_GPS_SPEED = "ddh:gps:speed"
