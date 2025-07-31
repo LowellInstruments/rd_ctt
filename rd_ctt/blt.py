e537953536ccf916490e36ca5aed16cb78ce25ab
kaz <kaz>
2025-07-31 09:36:01 -0400

    first

---
+++ rd_ctt/blt.py
@@ -0,0 +1,10 @@
+RD_BLT_GUI_ALIVE = "blt:th_alive"
+
+RD_BLT_BLE_SCAN_DUPLICATE = "blt:ble:results"
+RD_BLT_BLE_ANTENNA = "blt:ble:antenna"
+RD_BLT_BLE_TH_QUIT = "blt:th_quit:ble"
+
+RD_BLT_GPS_TH_QUIT = "blt:th_quit:gps"
+RD_BLT_GPS_FIX = "blt:gps:results"
+RD_BLT_GPS_ANTENNA = "blt:gps:antenna"
+RD_BLT_HAT_GFV = "blt:gps:hat_gfv"
