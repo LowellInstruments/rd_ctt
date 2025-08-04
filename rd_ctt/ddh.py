4b64b8245344f9f06a7afbfaab8e42306172a716
kaz <kaz>
2025-08-04 17:55:19 -0400

    more

--- rd_ctt/ddh.py
+++ rd_ctt/ddh.py
@@ -5,11 +5,13 @@ RD_DDH_GUI_PLOT = p + 'plot'
 
 
 p = 'ddh:ble:'
-RD_DDH_BLE_TH_QUIT = p + 'th_quit'
+RD_DDH_BLE_QUIT = p + 'quit'
+RD_DDH_BLE_FINISH_BOOT = p + 'boot'
+RD_DDH_BLE_ANTENNA = p + 'antenna'
 
 
 p = 'ddh:gps:'
-RD_DDH_GPS_TH_QUIT = p + 'th_quit'
+RD_DDH_GPS_QUIT = p + 'quit'
 RD_DDH_GPS_FIX = p + 'fix'
 RD_DDH_GPS_FINISH_BOOT = p + 'boot'
 RD_DDH_GPS_HAT_GFV = p + 'hat_gfv'
@@ -19,21 +21,21 @@ RD_DDH_GPS_ANTENNA = p + 'antenna'
 
 
 p = 'ddh:log:'
-RD_DDH_LOG_TH_QUIT = p + 'th_quit'
+RD_DDH_LOG_QUIT = p + 'quit'
 RD_DDH_LOG_QUEUE = p + 'queue'
 RD_DDH_LOG_FINISH_BOOT = p + 'boot'
 
 
 
 p = 'ddh:cnv:'
-RD_DDH_CNV_TH_QUIT = p + 'th_quit'
+RD_DDH_CNV_QUIT = p + 'quit'
 RD_DDH_CNV_FINISH_BOOT = p + 'boot'
 RD_DDH_CNV_QUEUE = p + 'queue'
 
 
 
 p = 'ddh:aws:'
-RD_DDH_AWS_TH_QUIT = p + 'th_quit'
+RD_DDH_AWS_QUIT = p + 'quit'
 RD_DDH_AWS_FINISH_BOOT = p + 'boot'
 RD_DDH_AWS_QUEUE = p + 'queue'
 RD_DDH_AWS_SYNC = p + 'sync'
