734b766208382ae1ad9368feefb5cf44accaf9dc
kaz <kaz>
2025-08-16 05:07:54 -0400

    more

---
+++ rd_ctt/lat.py
@@ -0,0 +1,18 @@
+p = 'lat:gui:'
+RD_LAT_GUI_ALIVE = p + 'alive'
+
+
+
+p = 'lat:ble:'
+RD_LAT_BLE_SCAN_DUPLICATE_RESULTS = p + 'results'
+RD_LAT_BLE_ANTENNA = p + 'antenna'
+RD_LAT_BLE_QUIT = p + 'th_quit'
+RD_LAT_BLE_TEMPLATE_TS_LAST_SEEN_MAC = p + 'mac_last_seen:{}'
+
+
+
+p = 'lat:gps:'
+RD_LAT_GPS_QUIT = p + 'th_quit'
+RD_LAT_GPS_FIX = p + 'results'
+RD_LAT_GPS_ANTENNA = p + 'antenna'
+RD_LAT_HAT_GFV = p + 'gfv_hat'
