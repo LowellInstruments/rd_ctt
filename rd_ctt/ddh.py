5486c188cf7360cba1c702587778f2de0c327645
kaz <kaz>
2025-08-18 20:55:43 -0400

    more

--- rd_ctt/ddh.py
+++ rd_ctt/ddh.py
@@ -1,26 +1,23 @@
 p = 'ddh:gui:'
-RD_DDH_GUI_ALIVE = p + 'alive'
 RD_DDH_GUI_PLOT = p + 'plot'
 RD_DDH_GUI_STATE_EVENT = p + 'state_event'
 RD_DDH_GUI_STATE_TEXT = p + 'state_text'
 RD_DDH_GUI_STATE_IMAGE = p + 'state_image'
-RD_DDH_GUI_ADD_HISTORY = p + 'history'
-RD_DDH_GUI_GRAPH_FOL_REQ = p + 'graph_fol_req'
-RD_DDH_GUI_HISTORY_REQ = p + 'history_req'
+RD_DDH_GUI_HISTORY_ADD = p + 'history'
+RD_DDH_GUI_HISTORY_REFRESH_REQ = p + 'history_refresh_req'
 
 
 
 
 p = 'ddh:ble:'
-RD_DDH_BLE_QUIT = p + 'quit'
 RD_DDH_BLE_FINISH_BOOT = p + 'boot'
 RD_DDH_BLE_ANTENNA = p + 'antenna'
 RD_DDH_BLE_RESET_REQ = p + 'linux_reset_req'
+RD_DDH_BLE_SEMAPHORE = p + 'semaphore'
 
 
 
 p = 'ddh:gps:'
-RD_DDH_GPS_QUIT = p + 'quit'
 RD_DDH_GPS_FIX = p + 'fix'
 RD_DDH_GPS_FINISH_BOOT = p + 'boot'
 RD_DDH_GPS_HAT_GFV = p + 'hat_gfv'
@@ -31,50 +28,38 @@ RD_DDH_GPS_TIME_AT_BOOT = p + 'time_boot'
 
 
 p = 'ddh:log:'
-RD_DDH_LOG_QUIT = p + 'quit'
 RD_DDH_LOG_QUEUE = p + 'queue'
 RD_DDH_LOG_FINISH_BOOT = p + 'boot'
 
 
 
 p = 'ddh:cnv:'
-RD_DDH_CNV_QUIT = p + 'quit'
 RD_DDH_CNV_FINISH_BOOT = p + 'boot'
 RD_DDH_CNV_QUEUE = p + 'queue'
 
 
 
 p = 'ddh:aws:'
-RD_DDH_AWS_QUIT = p + 'quit'
 RD_DDH_AWS_FINISH_BOOT = p + 'boot'
-RD_DDH_AWS_QUEUE = p + 'queue'
-RD_DDH_AWS_SYNC = p + 'sync'
+RD_DDH_AWS_COPY_QUEUE = p + 'queue'
 RD_DDH_AWS_STATE = p + 'state'
-RD_DDH_AWS_SEMAPHORE = p + 'semaphore'
+RD_DDH_AWS_SYNC = p + 'sync'
 
 
 
 p = 'ddh:sqs:'
-RD_DDH_SQS_QUIT = p + 'quit'
 RD_DDH_SQS_FINISH_BOOT = p + 'boot'
 RD_DDH_SQS_QUEUE = p + 'queue'
 
 
 
 p = 'ddh:net:'
-RD_DDH_NET_QUIT = p + 'quit'
 RD_DDH_NET_FINISH_BOOT = p + 'boot'
 RD_DDH_NET_FIX = p + 'fix'
 
 
 
-p = 'ddh:gfm:'
-RD_DDH_GFM_QUIT = p + 'quit'
-RD_DDH_GFM_FINISH_BOOT = p + 'boot'
-
-
-
 p = 'ddh:slo:'
-# to create more entries
+# to create more smart lock-out entries
 RD_DDH_SLO_LS = p + 'ls:'
 
