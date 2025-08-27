fda180b2247775b0b25aa4b70f0bb12dff4564c2
kaz <kaz>
2025-08-27 10:57:16 -0400

    more

--- rd_ctt/ddh.py
+++ rd_ctt/ddh.py
@@ -2,33 +2,34 @@ p = 'ddh:gui:'
 RD_DDH_GUI_PLOT = p + 'plot'
 RD_DDH_GUI_STATE_EVENT = p + 'state_event'
 RD_DDH_GUI_STATE_TEXT = p + 'state_text'
-RD_DDH_GUI_STATE_IMAGE = p + 'state_image'
 RD_DDH_GUI_HISTORY_ADD = p + 'history'
 p = p + 'refresh:'
-RD_DDH_GUI_REFRESH_HISTORY = p + 'state_history'
-RD_DDH_GUI_REFRESH_ANTENNA_BLE = p + 'state_antenna_ble'
-RD_DDH_GUI_REFRESH_ANTENNA_GPS = p + 'state_antenna_gps'
-RD_DDH_GUI_REFRESH_AWS = p + 'state_aws'
+RD_DDH_GUI_LOCK_ICON = p + 'lock_icon'
 RD_DDH_GUI_REFRESH_NET = p + 'state_net'
-RD_DDH_GUI_REFRESH_LAST_DL = p + 'state_last_dl'
+RD_DDH_GUI_REFRESH_AWS = p + 'state_aws'
+RD_DDH_GUI_REFRESH_GPS_ANTENNA = p + 'state_antenna_gps'
+RD_DDH_GUI_REFRESH_BLE_HISTORY = p + 'state_history'
+RD_DDH_GUI_REFRESH_BLE_ANTENNA = p + 'state_antenna_ble'
+RD_DDH_GUI_REFRESH_BLE_LAST_DL = p + 'state_last_dl'
 
 
 
 
 
 p = 'ddh:ble:'
-RD_DDH_BLE_FINISH_BOOT = p + 'boot'
 RD_DDH_BLE_RESET_REQ = p + 'linux_reset_req'
 RD_DDH_BLE_SEMAPHORE = p + 'semaphore'
+RD_DDH_BLE_FINISH_BOOT = p + 'boot'
 
 
 
 p = 'ddh:gps:'
-RD_DDH_GPS_FIX = p + 'fix'
-RD_DDH_GPS_FINISH_BOOT = p + 'boot'
+RD_DDH_GPS_FIX_POSITION = p + 'fix'
+RD_DDH_GPS_FIX_SPEED = p + 'speed'
+RD_DDH_GPS_FIX_NUMBER_OF_SATELLITES = p + 'ns_view'
+RD_DDH_GPS_COUNTDOWN_FOR_GPS_FIX_AT_BOOT = p + 'time_boot'
 RD_DDH_GPS_HAT_GFV = p + 'hat_gfv'
-RD_DDH_GPS_SPEED = p + 'speed'
-RD_DDH_GPS_TIME_AT_BOOT = p + 'time_boot'
+RD_DDH_GPS_FINISH_BOOT = p + 'boot'
 
 
 
@@ -39,21 +40,21 @@ RD_DDH_LOG_FINISH_BOOT = p + 'boot'
 
 
 p = 'ddh:cnv:'
-RD_DDH_CNV_FINISH_BOOT = p + 'boot'
 RD_DDH_CNV_QUEUE = p + 'queue'
+RD_DDH_CNV_FINISH_BOOT = p + 'boot'
 
 
 
 p = 'ddh:aws:'
-RD_DDH_AWS_FINISH_BOOT = p + 'boot'
 RD_DDH_AWS_COPY_QUEUE = p + 'queue'
-RD_DDH_AWS_SYNC = p + 'sync'
+RD_DDH_AWS_SYNC_REQUEST = p + 'sync'
+RD_DDH_AWS_FINISH_BOOT = p + 'boot'
 
 
 
 p = 'ddh:sqs:'
-RD_DDH_SQS_FINISH_BOOT = p + 'boot'
 RD_DDH_SQS_QUEUE = p + 'queue'
+RD_DDH_SQS_FINISH_BOOT = p + 'boot'
 
 
 
