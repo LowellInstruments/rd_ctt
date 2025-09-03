682e943b691ca11681063edb2a26e7b46d886c2e
kaz <kaz>
2025-09-03 05:33:46 -0400

    more

--- rd_ctt/ddh.py
+++ rd_ctt/ddh.py
@@ -5,16 +5,13 @@ RD_DDH_GUI_STATE_EVENT_TEXT = p + 'state_gui_event_text'
 RD_DDH_GUI_STATE_EVENT_ICON_LOCK = p + 'state_gui_event_icon_lock'
 RD_DDH_GUI_PLOT_REASON = p + 'plot_reason'
 RD_DDH_GUI_PLOT_FOLDER = p + 'plot_folder'
-p = 'ddh:gui:process:'
-RD_DDH_GUI_PROCESS_NET_OUTPUT = p + 'process_net_output'
-RD_DDH_GUI_PROCESS_AWS_OUTPUT = p + 'process_aws_output'
+RD_DDH_GUI_BOX_SIDE_BUTTON_TOP = p + 'side_button_top'
+RD_DDH_GUI_BOX_SIDE_BUTTON_MID = p + 'side_button_mid'
+RD_DDH_GUI_BOX_SIDE_BUTTON_LOW = p + 'side_button_low'
+
 p = 'ddh:gui:refresh:'
 RD_DDH_GUI_REFRESH_HISTORY_TABLE = p + 'history_table'
 RD_DDH_GUI_REFRESH_PROCESSES_PRESENT = p + 'processes_present'
-
-
-
-# todo ---> check these work
 RD_DDH_GUI_REFRESH_GPS_ANTENNA = p + 'state_antenna_gps'
 RD_DDH_GUI_REFRESH_BLE_ANTENNA = p + 'state_antenna_ble'
 RD_DDH_GUI_REFRESH_BLE_LAST_DL = p + 'state_last_dl'
@@ -30,51 +27,48 @@ RD_DDH_GUI_REFRESH_CELL_WIFI_ICON_AUTO = p + 'cell_wifi_icon'
 p = 'ddh:ble:'
 RD_DDH_BLE_RESET_REQ = p + 'linux_reset_req'
 RD_DDH_BLE_SEMAPHORE = p + 'semaphore'
-RD_DDH_BLE_FINISH_BOOT = p + 'boot'
 
 
 
 p = 'ddh:gps:'
 RD_DDH_GPS_FIX_POSITION = p + 'fix'
+RD_DDH_GPS_COUNTDOWN_FOR_FIX_AT_BOOT = p + 'countdown_for_fix_at_boot'
 RD_DDH_GPS_FIX_SPEED = p + 'speed'
 RD_DDH_GPS_FIX_NUMBER_OF_SATELLITES = p + 'ns_view'
-RD_DDH_GPS_COUNTDOWN_FOR_GPS_FIX_AT_BOOT = p + 'time_boot'
 RD_DDH_GPS_HAT_GFV = p + 'hat_gfv'
-RD_DDH_GPS_FINISH_BOOT = p + 'boot'
 
 
 
 p = 'ddh:log:'
 RD_DDH_LOG_QUEUE = p + 'queue'
-RD_DDH_LOG_FINISH_BOOT = p + 'boot'
 
 
 
 p = 'ddh:cnv:'
 RD_DDH_CNV_QUEUE = p + 'queue'
-RD_DDH_CNV_FINISH_BOOT = p + 'boot'
 
 
 
 p = 'ddh:aws:'
 RD_DDH_AWS_COPY_QUEUE = p + 'queue'
 RD_DDH_AWS_SYNC_REQUEST = p + 'sync'
-RD_DDH_AWS_FINISH_BOOT = p + 'boot'
+RD_DDH_GUI_PROCESS_AWS_OUTPUT = p + 'process_output'
+
 
 
 
 p = 'ddh:sqs:'
 RD_DDH_SQS_QUEUE = p + 'queue'
-RD_DDH_SQS_FINISH_BOOT = p + 'boot'
 
 
 
 p = 'ddh:net:'
-RD_DDH_NET_FINISH_BOOT = p + 'boot'
+RD_DDH_GUI_PROCESS_NET_OUTPUT = p + 'process_output'
 
 
 
 p = 'ddh:slo:'
 # to create more smart lock-out entries
 RD_DDH_SLO_LS = p + 'ls:'
+
 
